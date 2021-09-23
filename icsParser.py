37f1063a579aeb3f0ff242023aeee7a9fea046b8
MichOchieng <MichOchieng@gmail.com>
2021-09-23 00:12:25 -0700

    Can now parse targeted event attributes and create event objects from parsed data

--- icsParser.py
+++ icsParser.py
@@ -12,14 +12,17 @@ class Event:
 class Parser:
     # Constants used for identifying calendar event attributes
     
-    EVENT_BLOCK_START = "BEGIN:VEVENT"
-    EVENT_BLOCK_END   = "END:VEVENT"
+    EVENT_BLOCK_START  = "BEGIN:VEVENT"
+    EVENT_BLOCK_END    = "END:VEVENT"
 
-    EVENT_START_TIME  = "DTSTART;"
-    EVENT_END_TIME    = "DTEND;"
+    EVENT_START_TIME   = "DTSTART;"
+    EVENT_END_TIME     = "DTEND;"
 
-    EVENT_DESCRIPTION = "DESCRIPTION:"
-    EVENT_SUMMARY     = "SUMMARY:"
+    EVENT_DESCRIPTION  = "DESCRIPTION:"
+    EVENT_SUMMARY      = "SUMMARY:"
+
+    EVENT_DESC_END     = ["LAST-MODIFIED:","LOCATION:","SEQUENCE:","STATUS:","SUMMARY:"]
+    EVENT_DATA_REMAINS = "\n\nî¡¸\n\n\nOrganiser: News Blocks"
     
     # Will hold event objects before they are sent to a file
     EVENT_LIST = []
@@ -38,8 +41,9 @@ class Parser:
         # Takes in file as a command line argument
         with open(sys.argv[1],"r") as file:
             lines = file.readlines()
-        # Parse file data into event objects
-        eventFound = False
+
+        eventFound  = False
+        readingDesc = False # Allows the full description to be parsed if it spans multiple lines
 
         # Temporary values used to create event objects at the end of for loop
         tempStartTime = ''
@@ -51,32 +55,54 @@ class Parser:
             if(line.replace('\n','') == self.EVENT_BLOCK_START): # Removing new line char at the end of each line
                 eventFound = True
                 continue
+
             # Get event start time
             if(self.EVENT_START_TIME in line and eventFound):
                 # Strip date/time from string
                 time = re.sub('[^0-9]','',line)
-                print(self.parseDateTime(time))
+                tempStartTime = self.parseDateTime(time)
                 continue
+
             # Get event end time
             if(self.EVENT_END_TIME in line and eventFound):
                 # Strip date/time from string
                 time = re.sub('[^0-9]','',line)
-                print(self.parseDateTime(time))
+                tempEndTime = self.parseDateTime(time)
                 continue
+
             # Get event description
             if(self.EVENT_DESCRIPTION in line and eventFound):
-                print(line)
+                readingDesc = True
+                desc = line.replace(self.EVENT_DESCRIPTION,'')
+                tempDesc = desc.replace('\\n','')
                 continue
+            
+            # Stops unwanted event attributes after the description from being saved to event object
+            if(readingDesc and eventFound and not any(identifier in line for identifier in self.EVENT_DESC_END )):
+                tempDesc += line.replace('\n','')
+
             # Get event summary
             if(self.EVENT_SUMMARY in line and eventFound):
-                # Push event to list
-                print(line)
-                continue 
-            if(self.EVENT_BLOCK_END in line and eventFound):
-                eventFound = False
-                # Push event to list
-                print(line)
+                readingDesc = False
+                # tempSum = re.sub('[^a-zA-Z ]','',(line.replace(self.EVENT_SUMMARY,''))) will only display alphabetical characters and spaces
+                tempSum = (line.replace(self.EVENT_SUMMARY,'')).replace('\n','')
                 continue 
 
+            if(self.EVENT_BLOCK_END in line and eventFound):
+                eventFound = False
+                # Create event object
+                temp = Event(tempSum,tempDesc,tempStartTime,tempEndTime)
+                # Push event to list
+                self.EVENT_LIST.append(temp)
+                continue 
+    def viewEvents(self):
+        for evnt in self.EVENT_LIST:
+            print("---------")
+            print(evnt.name)  
+            print(evnt.description)
+            print(evnt.startTime)
+            print(evnt.endTime)          
+            print("---------")
 p = Parser()
 p.parseFile()
+p.viewEvents()
